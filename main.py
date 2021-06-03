eec92c5153019403c07e4fffaff10eed57902c99
Matthew Searfass <mattsea1028@gmail.com>
2021-06-03 11:09:09 -0400

    CSV parser working

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import csv
 
 validPos = 0.075
 validNeg = 0.15
+
 
 def posControl(PC1, PC2):
     return
@@ -17,35 +19,42 @@ def calcSP():
 def calcTiter():
     return
 
+def readCSV():
+    csvList = []
+    ifile  = open('csv/test.csv', "r")
+    read = csv.reader(ifile)
+    for row in read:
+
+        for i in row:
+            if(any(map(str.isdigit, i))):
+                csvList.append(float(i))
+
+    return csvList
+    
 
 
-
+print(readCSV())
 # frequencies
 ages = [3, 5, 3, 3]
   
-    # setting the ranges and no. of intervals
+
 range = (0, 18)
 bins = 18  
-  
-    # plotting a histogram
+
 plt.hist(ages, bins, range, color = 'blue',
         histtype = 'bar', rwidth = 0.8)
-  
-    # x-axis label
+
 plt.xlabel('Titer Groups')
-    # frequency label
+
 plt.ylabel('Count')
-    # plot title
 plt.title('PC TURKEY SERA - REO')
 
 ax = plt.subplot()
 
 #I know, I know. But theres no support for range()
 plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
-    # function to show the plot
 ax.set_xticklabels('')
 
-# Customize minor tick labels
 ax.set_xticks([1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5,13.5,14.5,15.5,16.5,17.5,18.5],      minor=True)
 ax.set_xticklabels(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18'], minor=True)
 plt.show()
