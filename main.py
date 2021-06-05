bf50ad49a390eb104ded1558628d7fb294f4f8e2
Matthew Searfass <mattsea1028@gmail.com>
2021-06-05 12:04:14 -0400

    Updated valididty controls

--- main.py
+++ main.py
@@ -7,15 +7,22 @@ validNeg = 0.15
 
 
 def posControl(PC1, PC2):
-    return
+    return ((PC1 + PC2) / 2)
+
 def negControl(NC1, NC2):
-    return
-def isValidPos(PCX):
-    return
-def isValidNeg(NCX):
-    return
+    return((NC1 + NC2) / 2)
+
+def isValid(PCX, NCX):
+    if((PCX - NCX) > validPos and (NCX <= validNeg)):
+        return True
+    else: 
+        return False
+
+
+
 def calcSP():
     return
+
 def calcTiter():
     return
 
@@ -55,8 +62,8 @@ ax = plt.subplot()
 plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
 ax.set_xticklabels('')
 
-ax.set_xticks([1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5,13.5,14.5,15.5,16.5,17.5,18.5],      minor=True)
-ax.set_xticklabels(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18'], minor=True)
+ax.set_xticks([0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5,13.5,14.5,15.5,16.5,17.5,18.5],      minor=True)
+ax.set_xticklabels(['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18'], minor=True)
 plt.show()
 
 print("done")
