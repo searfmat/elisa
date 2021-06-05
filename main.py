8e1bfbd107f7c40ec4f5c5f45a2055d232e8143d
Matthew Searfass <mattsea1028@gmail.com>
2021-06-05 13:50:08 -0400

    Fixed bin 18 bug

--- main.py
+++ main.py
@@ -101,6 +101,7 @@ def main():
             ods.append(vals[k])
 
     for j in ods:
+
         titer = calcTiter(calcSP(j, NC, PC))
         if(titer == 0):
             titerGroup.append(0)
@@ -122,11 +123,30 @@ def main():
             titerGroup.append(8)
         elif(titer < 9000):
             titerGroup.append(9)
+        elif(titer < 10000):
+            titerGroup.append(10) 
+        elif(titer < 11000):
+            titerGroup.append(11)  
+        elif(titer < 12000):
+            titerGroup.append(12)  
+        elif(titer < 13000):
+            titerGroup.append(13)  
+        elif(titer < 14000):
+            titerGroup.append(14)  
+        elif(titer < 15000):
+            titerGroup.append(15)  
+        elif(titer < 16000):
+            titerGroup.append(16)  
+        elif(titer < 17000):
+            titerGroup.append(17)  
         else:
-            titerGroup.append(10)    
+            titerGroup.append(18)
 
-    r = (0, 18)
-    bins = 18  
+    
+
+
+    r = (0, 19)
+    bins = 19  
 
     plt.hist(titerGroup, bins, r, color = 'blue',
             histtype = 'bar', rwidth = 0.8)
@@ -139,7 +159,7 @@ def main():
     ax = plt.subplot()
 
     #I know, I know. But theres no support for range()
-    plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
+    plt.xticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
     ax.set_xticklabels('')
 
     ax.set_xticks([0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5,13.5,14.5,15.5,16.5,17.5,18.5],      minor=True)
